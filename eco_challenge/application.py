c6c916816fcef96a71297e2e151119047c6107da
volodymyrb <volodymyr.borysiuk0@gmail.com>
2023-07-28 17:33:10 +0300

    ✨ Add Docker support and update DB host in config.

--- eco_challenge/application.py
+++ eco_challenge/application.py
@@ -1,11 +1,11 @@
-from fastapi import FastAPI
-
-
 import contextlib
 
-from eco_challenge.core import routers
-from .core.database import DATA_BASE
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
 from . import quiz_app, daily_task_app
+from .core.database import DATA_BASE
+from eco_challenge.core import routers
 
 
 @contextlib.asynccontextmanager
@@ -16,6 +16,7 @@ async def lifespan(_: FastAPI):
 
 def create_app() -> FastAPI:
     app = FastAPI(lifespan=lifespan)
+
     app.include_router(routers.user_router.router)
     app.include_router(routers.role_router.router)
     app.include_router(routers.points_count_router.router)
@@ -23,4 +24,13 @@ def create_app() -> FastAPI:
     app.include_router(routers.auth_router.router)
     app.include_router(quiz_app.router)
     app.include_router(daily_task_app.router)
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=['*'],
+        allow_methods=['*'],
+        allow_headers=['*'],
+        allow_credentials=True,
+    )
+
     return app
